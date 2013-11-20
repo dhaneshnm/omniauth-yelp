6f02373f9a89a33eb7222322665fdc3081e84c99
dragon364 <dhanesh.n.m19@gmail.com>
2013-11-20 16:19:30 -0500

    created strategy for yelp

---
+++ lib/omniauth/strategies/yelp.rb
@@ -0,0 +1,73 @@
+require 'omniauth-oauth2'
+require 'cgi'
+require 'uri'
+require 'oauth2'
+require 'omniauth'
+require 'timeout'
+require 'securerandom'
+require 'openssl'
+require 'base64'
+require 'cgi'
+#require 'hmac-sha1'
+require "logger"
+
+require 'net/http'
+module OmniAuth
+  module Strategies
+    class Yelp
+      include OmniAuth::Strategy
+      option :client_options, {:site =>"api.yelp.com"}
+      def request_phase
+          url = 'http://api.yelp.com/v2/search?'+request.query_string
+          parsed_url = URI.parse( url )
+          Net::HTTP.start( parsed_url.host ) { | http |
+            params = sign(parsed_url,'GET')
+            signed_req = get_query_string(params)
+            req = Net::HTTP::Get.new "#{ parsed_url.path }?#{ signed_req }"
+            response = http.request(req)
+            ENV["YelpData"] = response.read_body
+            re_url = callback_url
+            redirect re_url
+          }
+      end
+      def percent_encode( string )
+        return URI.escape( string, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]") ).gsub('*', '%2A')
+      end
+      def signature base_str
+          key = percent_encode( options[:consumer_secret] ) + '&' + percent_encode( options[:token_secret] )
+          digest = OpenSSL::Digest::Digest.new( 'sha1' )
+          hmac = OpenSSL::HMAC.digest( digest, key, base_str )
+          Base64.encode64( hmac ).chomp.gsub( /\n/, '' )
+      end
+      def get_query_string params
+          pairs = []
+          params.sort.each { | key, val | 
+            pairs.push( "#{ percent_encode( key ) }=#{ percent_encode( val.to_s ) }" )
+          }
+          pairs.join '&'
+      end
+      def sign( parsed_url ,req_method)
+          params = {
+            'oauth_consumer_key' => options[:consumer_key],
+            'oauth_token' =>options[:token],
+            'oauth_nonce' => Array.new( 5 ) { rand(256) }.pack('C*').unpack('H*').first,
+            'oauth_signature_method' => 'HMAC-SHA1',
+            'oauth_timestamp' => Time.now.to_i.to_s,
+            'oauth_version' =>'1.0'
+          }
+          if parsed_url.query
+            params.merge! CGI.parse( parsed_url.query )
+          end
+          req_url = parsed_url.scheme + '://' + parsed_url.host + parsed_url.path
+          base_str = [ 
+            req_method, 
+            percent_encode( req_url ),
+            percent_encode( get_query_string(params) ) 
+            
+          ].join( '&' )
+          params[ 'oauth_signature' ] = signature(base_str)
+          return params
+      end
+    end
+  end
+end
--- omniauth-yelp.gemspec
+++ omniauth-yelp.gemspec
@@ -14,39 +14,13 @@ Gem::Specification.new do |s|
   s.require_paths = ["lib"]
   s.files         = `git ls-files`.split("\n")
   s.license       = "MIT"
-  s.date        = '2014-10-23'
+  s.date        = '2014-11-23'
   s.summary     = "omniauth-yelp!"
   s.description = "A simple  omniauth gem"
   s.authors     = ["Dhanesh Neela Mana"]
   s.email       = 'dhanesh.n.m19@gmail.com'
-  s.files       = `git ls-files`.split("\n") #["lib/omniauth-yelp.rb","lib/omniauth-yelp/translator.rb","lib/omniauth-yelp/omniauth/yelp.rb","lib/omniauth-yelp/omniauth/strategies/yelp.rb","lib/omniauth-yelp/omniauth/yelp/version.rb"]
-  s.homepage    = 'https://github.com/intridea/omniauth'
-  s.license       = 'https://github.com/intridea/omniauth/blob/master/LICENSE.md'
+  s.files       = ["lib/omniauth-yelp.rb","lib/omniauth/yelp.rb","lib/omniauth/strategies/yelp.rb","lib/omniauth/yelp/version.rb"]#git ls-files`.split("\n") 
+  s.homepage    = ''
+  s.license       = ''
 end
-#-*- encoding: utf-8 -*-
-#require File.expand_path('../lib/omniauth-oauth2/version', __FILE__)
 
-# Gem::Specification.new do |gem|
-#   gem.add_dependency 'omniauth', '~> 1.0'
-#   gem.add_dependency 'omniauth-oauth2', '~> 1.0'
-#   gem.add_dependency 'oauth2', '~> 0.9.0'
-
-#   gem.add_development_dependency 'rspec', '~> 2.7'
-#   gem.add_development_dependency 'rack-test'
-#   gem.add_development_dependency 'webmock'
-#   gem.add_development_dependency 'simplecov'
-
-#   gem.authors       = ["Dhanesh Neela Mana"]
-#   gem.email         = ["dhanesh.n.m19@gmail.com"]
-#   gem.description   = "omniauth-yelp!" #%q{An abstract OAuth2 strategy for OmniAuth.}
-#   gem.summary       = "omniauth-yelp!" #%q{An abstract OAuth2 strategy for OmniAuth.}
-#   #gem.homepage      = "https://github.com/intridea/omniauth-oauth2"
-#   gem.license       = "MIT"
-
-#   #gem.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
-#   #gem.files         = `git ls-files`.split("\n")
-#   #gem.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
-#   gem.name          = "omniauth-yelp"
-#   gem.require_paths = ["lib"]
-#   gem.version       = "0.0.0"
-# end
