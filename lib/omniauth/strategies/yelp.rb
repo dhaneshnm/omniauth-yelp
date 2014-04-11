06c0c3b611f157346fbc79a0a339f15b9a4e4fea
dragon364 <dhanesh.n.m19@gmail.com>
2014-04-11 14:28:46 -0400

    moved all code to call back phase

--- lib/omniauth/strategies/yelp.rb
+++ lib/omniauth/strategies/yelp.rb
@@ -5,6 +5,12 @@ module OmniAuth
     class Yelp
       include OmniAuth::Strategy
       def request_phase
+        redirect callback_url
+      end
+      extra do
+        JSON.parse(@yelp_data_string)
+      end
+      def callback_phase
         consumer_key = options[:consumer_key]
         consumer_secret = options[:consumer_secret]
         oauth_token = options[:token]
@@ -12,23 +18,12 @@ module OmniAuth
         consumer = OAuth::Consumer.new(consumer_key, consumer_secret, {
                                 :site               => 'http://api.yelp.com',
                                  :scheme             => :query_string,
-                                 :http_method        => :get,
-                                 :request_token_path => "/v2/search",
-                                 :access_token_path  => "/v2/search",
-                                 :authorize_path     => "/v2/search"
+                                 :http_method        => :get
                                })
-        path  = "/v2/search?term=noodle&location=Philadelphia"
+        path  = "/v2/search?"+request.query_string
         this_token = OAuth::Token.new(oauth_token,token_secret)
         response = consumer.request(:get,path,this_token, { :scheme => :query_string })
-        ENV["YelpData"] = response.body
         @yelp_data_string = response.body
-        puts callback_url
-        redirect callback_url
-      end
-      extra do
-        JSON.parse(ENV["YelpData"])
-      end
-      def callback_phase
         super
       end
     end
